324ca801cdb65d1193a4995b45409fb051542cd4
Thaddeus Teh :D <thaddeusteh2@gmail.com>
2025-11-29 17:02:05 +0800

    implement dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization")
+    alias(libs.plugins.google.gms.google.services)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -49,6 +53,42 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+//    implementation("androidx.compose.material:material-icons-core:1.7.8")
+//    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+//
+//    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.4")
+//
+//    implementation("androidx.navigation:navigation-compose:2.9.6")
+//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+//    implementation(libs.firebase.firestore)
+//
+//    implementation("com.google.dagger:hilt-android:2.57.2")
+//    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+//    ksp("com.google.dagger:hilt-compiler:2.57.2")
+
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material.icons.extended)
+
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.firebase.firestore)
+
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.volley)
+    implementation(libs.firebase.database)
+    ksp(libs.hilt.compiler)
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
