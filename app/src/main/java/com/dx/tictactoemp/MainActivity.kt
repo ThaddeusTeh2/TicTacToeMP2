fe26f545a525864ada0f390b92281cd89af5f9ef
Thaddeus Teh :D <thaddeusteh2@gmail.com>
2025-11-29 17:04:56 +0800

    implement step 9: update MainActivity.kt w Nav

--- app/src/main/java/com/dx/tictactoemp/MainActivity.kt
+++ app/src/main/java/com/dx/tictactoemp/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.dx.tictactoemp.navigation.Screen
+import com.dx.tictactoemp.ui.screen.CreateRoomScreen
+import com.dx.tictactoemp.ui.screen.GameScreen
+import com.dx.tictactoemp.ui.screen.HomeScreen
+import com.dx.tictactoemp.ui.screen.JoinRoomScreen
 import com.dx.tictactoemp.ui.theme.TicTacToeMPTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +28,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TicTacToeMPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TicTacToeApp()
                 }
             }
         }
@@ -31,17 +40,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TicTacToeApp() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TicTacToeMPTheme {
-        Greeting("Android")
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(
+                onCreateRoom = {
+                    navController.navigate(Screen.CreateRoom.route)
+                },
+                onJoinRoom = {
+                    navController.navigate(Screen.JoinRoom.route)
+                }
+            )
+        }
+
+        composable(Screen.CreateRoom.route) {
+            CreateRoomScreen(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigateToGame = { roomId ->
+                    navController.navigate(Screen.Game.createRoute(roomId)) {
+                        popUpTo(Screen.Home.route)
+                    }
+                }
+            )
+        }
+
+        composable(Screen.JoinRoom.route) {
+            JoinRoomScreen(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigateToGame = { roomId ->
+                    navController.navigate(Screen.Game.createRoute(roomId)) {
+                        popUpTo(Screen.Home.route)
+                    }
+                }
+            )
+        }
+
+        composable(
+            route = Screen.Game.route,
+            arguments = listOf(
+                navArgument("roomId") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val roomId = backStackEntry.arguments?.getString("roomId") ?: return@composable
+            GameScreen(
+                roomId = roomId,
+                onBack = {
+                    navController.popBackStack(Screen.Home.route, inclusive = false)
+                }
+            )
+        }
     }
 }
