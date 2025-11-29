e070eb88cd96306fcff2c7eea09417cff85b822b
Thaddeus Teh :D <thaddeusteh2@gmail.com>
2025-11-29 18:59:51 +0800

    implement RepositoryProvider.kt

--- app/src/main/java/com/dx/tictactoemp/di/RepositoryProvider.kt
+++ app/src/main/java/com/dx/tictactoemp/di/RepositoryProvider.kt
@@ -1,15 +1,47 @@
 package com.dx.tictactoemp.di
 
-import com.dx.tictactoemp.data.repository.AuthRepository
-import com.dx.tictactoemp.data.repository.GameRepository
-import com.dx.tictactoemp.data.repository.LocalAuthRepository
-import com.dx.tictactoemp.data.repository.LocalGameRepository
+import android.content.Context
+import com.dx.tictactoemp.data.repository.*
 
 object RepositoryProvider {
-    private val authRepository: AuthRepository by lazy { LocalAuthRepository() }
-    private val gameRepository: GameRepository by lazy { LocalGameRepository() }
+    private var context: Context? = null
+    private var useFirebase: Boolean = false
 
-    fun provideAuthRepository(): AuthRepository = authRepository
-    fun provideGameRepository(): GameRepository = gameRepository
+    private val localAuthRepository: AuthRepository by lazy { LocalAuthRepository() }
+    private val localGameRepository: GameRepository by lazy { LocalGameRepository() }
+
+    private val firebaseAuthRepository: FirebaseAuthRepository? by lazy {
+        context?.let { FirebaseAuthRepository(it) }
+    }
+    private val firebaseGameRepository: GameRepository by lazy { FirebaseGameRepository() }
+
+    fun initialize(appContext: Context, firebase: Boolean = false) {
+        context = appContext
+        useFirebase = firebase
+    }
+
+    fun setFirebaseMode(enabled: Boolean) {
+        useFirebase = enabled
+    }
+
+    fun isFirebaseMode(): Boolean = useFirebase
+
+    fun provideAuthRepository(): AuthRepository {
+        return if (useFirebase && firebaseAuthRepository != null) {
+            firebaseAuthRepository!!
+        } else {
+            localAuthRepository
+        }
+    }
+
+    fun provideGameRepository(): GameRepository {
+        return if (useFirebase) {
+            firebaseGameRepository
+        } else {
+            localGameRepository
+        }
+    }
+
+    fun provideFirebaseAuthRepository(): FirebaseAuthRepository? = firebaseAuthRepository
 }
 
